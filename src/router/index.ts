ba6c9aac6cd9ad196d611aee2a3b18c7f5e9d108
sehee-xx <104626753+sehee-xx@users.noreply.github.com>
2025-05-09 10:31:56 +0900

    Modify: 지역별 리스트 라우팅 추가

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,11 +2,18 @@ import { createRouter, createWebHistory } from "vue-router";
 import Landing from "../views/Landing.vue";
 import Join from "../views/Join.vue";
 import List from "../views/List.vue";
+import DestinationPlans from "../views/DestinationPlans.vue";
 
 const routes = [
   { path: "/", component: Landing },
   { path: "/join", component: Join },
   { path: "/list", component: List },
+  {
+    path: "/list/:regionId",
+    name: "DestinationPlans",
+    component: DestinationPlans,
+    props: true, // regionId 를 props 로 전달
+  },
 ];
 
 export default createRouter({
