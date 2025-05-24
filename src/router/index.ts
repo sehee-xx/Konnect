2ce823d1bd19e45a59598a8e87ab6cb4a14e0d54
sehee-xx <104626753+sehee-xx@users.noreply.github.com>
2025-05-25 01:40:25 +0900

    Modify: Mypage, trip-create routing 추가

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,43 @@
+// src/router/index.js
 import { createRouter, createWebHistory } from "vue-router";
 import Landing from "../views/Landing.vue";
 import Join from "../views/Join.vue";
 import List from "../views/List.vue";
+import Mypage from "../views/Mypage.vue";
 import DestinationPlans from "../views/DestinationPlans.vue";
 import CardDetail from "../views/CardDetail.vue";
+import TripCreate from "../views/TripCreate.vue";
 
 const routes = [
-  { path: "/", component: Landing },
-  { path: "/join", component: Join },
+  { path: "/", component: Landing, meta: { noHeader: true } },
+  { path: "/join", component: Join, meta: { noHeader: true } },
   { path: "/list", component: List },
+  {
+    path: "/mypage",
+    name: "Mypage",
+    component: Mypage,
+  },
+  {
+    path: "/trip-create",
+    name: "TripCreate",
+    component: TripCreate,
+  },
   {
     path: "/list/:regionId",
     name: "DestinationPlans",
     component: DestinationPlans,
-    props: true, // regionId 를 props 로 전달
+    props: true,
   },
   {
     path: "/plan/:planId",
     name: "PlanById",
     component: CardDetail,
+    props: true,
+  },
+  {
+    path: "/plan/:planId/edit",
+    name: "PlanEdit",
+    component: () => import("../views/TravelEdit.vue"),
     props: true,
   },
 ];
