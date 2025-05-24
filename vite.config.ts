8bcaffcbb71334d1396e45e1ef96050cf88ea0c6
sehee-xx <104626753+sehee-xx@users.noreply.github.com>
2025-05-25 01:39:39 +0900

    Modify: api root 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  server: {
+    proxy: {
+      "/oauth2": "http://20.200.137.41:8080",
+      "/login": "http://20.200.137.41:8080",
+      "/signup": "http://20.200.137.41:8080",
+      "/api": {
+        target: "http://20.200.137.41:8080",
+        changeOrigin: true,
+        // 필요한 경우 rewrite 로직을 넣어주세요
+        // rewrite: path => path.replace(/^\/api/, "/api")
+      },
+    },
+  },
+});
